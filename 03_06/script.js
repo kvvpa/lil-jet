8f4c919d22827f31d443808d403d36055e86215b
kvvpa <hi@kvvpa.com>
2022-05-17 11:54:23 -0700

    complete up until 03_07

--- 03_06/script.js
+++ 03_06/script.js
@@ -22,3 +22,9 @@ const backpack = {
 };
 
 console.log("The backpack object:", backpack);
+console.log("the pocketNum value:", backpack.pocketNum);
+console.log("strap length L:", backpack.strapLength.left);
+
+var query = "pocketNum";
+
+console.log("the pocketNum value", backpack[query]);
